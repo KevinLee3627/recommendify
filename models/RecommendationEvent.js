b8bbc96559117ef0dc0f11bd511776a66d283f8b
Kevin Lee <kevinlee3627@gmail.com>
2020-07-09 17:38:05 -0500

    Deleted RecommendationEvent model

--- models/RecommendationEvent.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-let RecommendationEventSchema = new mongoose.Schema({
-	spotify_id: String,
-	obj_name: String,
-	date_recommended: Date
-});
-
-module.exports = mongoose.model('RecommendationEvent', RecommendationEventSchema)
