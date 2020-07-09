df354f6df4a0bfdb590418b9018ed8b95347b926
Kevin Lee <kevinlee3627@gmail.com>
2020-07-09 17:36:41 -0500

    Adding field to Recommendation model

--- models/Recommendation.js
+++ models/Recommendation.js
@@ -5,7 +5,8 @@ const mongoose_random = require('mongoose-random');
 let RecommendationSchema = new mongoose.Schema({
 	spotify_id: String, //not visible to clients
 	obj_data: {},
-	times_recommended: Number
+	times_recommended: Number,
+	dates_recommended: Array
 });
 RecommendationSchema.plugin(mongoose_random, {path: 'r'});
 module.exports = mongoose.model('Recommendation', RecommendationSchema);
